9ca9f14936fe85809c3c33f1f5d1eb6b0e47504a
fmachta <fmachta88@gmail.com>
2023-11-21 19:29:21 -0800

    Udated files
    Updated files

--- config.js
+++
@@ -1,5 +0,0 @@
-//  const config = {
-//    GOOGLE_MAPS_API_KEY: 'AIzaSyA60I_LmyGxTmeRHLbxQ3FuIVqpLj-Bf0Y'
-//};
-
-<script aync src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA60I_LmyGxTmeRHLbxQ3FuIVqpLj-Bf0Y&callback=initMap&libraries=maps&v=beta"></script>
